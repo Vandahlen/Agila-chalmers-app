1a1e9a7dd2ddac2700eec838ed9331fb57c7261d
Jakob <jakobah@chalmer.se>
2026-08-02 15:06:20 +0200

    fix: remove duplicated babel.config.js export, add missing @env type declaration

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,5 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-};
-module.exports = {
-  presets: ['module:@react-native/babel-preset'],
   plugins: [
     ['module:react-native-dotenv'],
   ],
